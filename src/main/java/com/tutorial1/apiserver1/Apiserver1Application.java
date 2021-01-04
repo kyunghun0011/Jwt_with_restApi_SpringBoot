d3286a02e66f7315aeadebbad249ffbbaba503ef
kyunghun0011 <kyunghun0011@gmail.com>
2021-01-04 23:38:37 +0900

    Initial commit

---
+++ src/main/java/com/tutorial1/apiserver1/Apiserver1Application.java
@@ -0,0 +1,13 @@
+package com.tutorial1.apiserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apiserver1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apiserver1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tutorial1/apiserver1/Apiserver1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial1.apiserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apiserver1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
